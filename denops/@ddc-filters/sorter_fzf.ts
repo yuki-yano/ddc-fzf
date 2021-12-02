c974cff269160fd6a95bd796b8bb03c391387274
yuki_yano <yuki.ycino@gmail.com>
2021-12-02 17:34:45 +0900

    first commit

---
+++ denops/@ddc-filters/sorter_fzf.ts
@@ -0,0 +1,33 @@
+import {
+  BaseFilter,
+  Candidate,
+} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts#^";
+
+export class Filter extends BaseFilter<{}> {
+  async filter(args: {
+    completeStr: string;
+    candidates: Candidate[];
+  }): Promise<Candidate[]> {
+    const input = args.completeStr;
+
+    const p = Deno.run({
+      cmd: ["fzf", "--filter", input],
+      stdin: "piped",
+      stdout: "piped",
+    });
+
+    await p.stdin.write(
+      new TextEncoder().encode(args.candidates.map((c) => c.word).join("\n")),
+    );
+    p.stdin.close();
+
+    const output = new TextDecoder().decode(await p.output());
+    p.close();
+
+    return output.split("\n").map((v) => ({ word: v }));
+  }
+
+  params(): {} {
+    return {};
+  }
+}
